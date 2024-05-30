d6a7cdee24d266217fd8bef949d67b235878833c
Rennan Fernandes <140443000+rennanfb@users.noreply.github.com>
2024-05-30 00:18:42 -0300

    Delete FirstConflict.hpp
    useless old code

--- FirstConflict.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include "Warrior.hpp"
-#include "Rogue.hpp"
-#include "Mage.hpp"
-#include "NpCharacter.hpp"
-#include "Goblin.hpp"
-
-void FirstConflict(Character* player1, NpCharacter* enemy);
