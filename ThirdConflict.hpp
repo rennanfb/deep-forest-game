20895f14eedc8d866f68568f79b04b51f7c74b28
Rennan Fernandes <140443000+rennanfb@users.noreply.github.com>
2024-05-30 00:17:33 -0300

    Delete ThirdConflict.hpp
    useless old code

--- ThirdConflict.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include "Warrior.hpp"
-#include "Rogue.hpp"
-#include "Mage.hpp"
-#include "NpCharacter.hpp"
-#include "Goblin.hpp"
-
-void ThirdConflict(Character* player1, NpCharacter* enemy);
